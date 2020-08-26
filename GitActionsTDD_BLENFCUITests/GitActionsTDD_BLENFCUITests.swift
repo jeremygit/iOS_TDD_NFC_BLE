de16d1d9e8eaac787c2dc39baf8f4c92d7d7c08b
jeremygit <jheritage@gmail.com>
2020-08-26 22:40:40 +1000

    setup a basic test

--- GitActionsTDD_BLENFCUITests/GitActionsTDD_BLENFCUITests.swift
+++ GitActionsTDD_BLENFCUITests/GitActionsTDD_BLENFCUITests.swift
@@ -9,27 +9,24 @@
 import XCTest
 
 class GitActionsTDD_BLENFCUITests: XCTestCase {
+    
+    private var app: XCUIApplication = XCUIApplication()
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        self.app.launch()
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testHelloWorldUITest() throws {
+        let helloWorldText = self.app.staticTexts["helloWorldText"]
+        XCTAssertEqual("Hello, World!", helloWorldText.label)
     }
 
     func testLaunchPerformance() throws {
