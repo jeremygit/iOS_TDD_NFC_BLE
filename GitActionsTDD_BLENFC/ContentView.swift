c7f9f4b17a4764039af8ca8dc4b57a0589f04bff
jeremygit <jheritage@gmail.com>
2020-08-26 22:20:25 +1000

    initial add

---
+++ GitActionsTDD_BLENFC/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitActionsTDD_BLENFC
+//
+//  Created by Jeremy Heritage on 26/8/20.
+//  Copyright © 2020 Jeremy Heritage. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitActionsTDD_BLENFCTests/GitActionsTDD_BLENFCTests.swift
@@ -0,0 +1,34 @@
+//
+//  GitActionsTDD_BLENFCTests.swift
+//  GitActionsTDD_BLENFCTests
+//
+//  Created by Jeremy Heritage on 26/8/20.
+//  Copyright © 2020 Jeremy Heritage. All rights reserved.
+//
+
+import XCTest
+@testable import GitActionsTDD_BLENFC
+
+class GitActionsTDD_BLENFCTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
